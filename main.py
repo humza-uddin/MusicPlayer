eeccdbeaf46266a8546d676d046af9a767ecc648
Humza Uddin <thereal.humza@gmail.com>
2023-08-02 01:04:39 +0500

    main.py
    Made the GUI more visually appealing

--- main.py
+++ main.py
@@ -1,29 +1,82 @@
 from tkinter import filedialog
 from tkinter import * 
+import customtkinter
 import pygame
 import os
 
-window = Tk()
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+window = customtkinter.CTk()
 window.title('Music Player')
-window.geometry("600x400")
+window.geometry("620x420")
 
 pygame.mixer.init()
 
 menubar= Menu(window)
 window.config(menu=menubar)
 
-organise_menu = Menu(menubar)
-organise_menu.add_command(label='Select Folder')
-menubar.add_cascade(label='Organise',menu=organise_menu)
+songs = []
+current_song = ""
+pause = False
 
-def browser_button():
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
-    print(filename)
+def load_music():
+    global current_song
+    window.directory = filedialog.askdirectory()
+    
+    for song in os.listdir(window.directory):
+        name, ext = os.path.splitext(song)
+        if ext == '.mp3':
+            songs.append(song)
 
-folder_path = StringVar()
-lbl1 = Label(master=window, textvariable = folder_path)
-button2 = Button(text="Browse", command=browser_button)
+    for song in songs:
+        songlist.insert("end", song)
+
+    songlist.selection_set(0)
+    current_song = songs[songlist.curselection()[0]] 
+
+def play_music():
+    global current_song, pause
+
+    if not pause:
+        pygame.mixer.music.load(os.path.join(window.directory, current_song))
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.unpause()
+        pause = False
+
+def pause_music():
+    global pause
+    pygame.mixer.music.pause()
+    pause = True
+    
+
+def next_music():
+    global current_song, pause
+
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) + 1)
+        current_song = songs(songlist.curselection()[0])
+        play_music()
+    except:
+        pass
+
+
+def previous_music():
+    global current_song, pause
+
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) - 1)
+        current_song = songs(songlist.curselection()[0])
+        play_music()
+    except:
+        pass
+
+browse_menu = Menu(menubar,tearoff=False)
+browse_menu.add_command(label='Select Folder', command= load_music)
+menubar.add_cascade(label='Browse',menu=browse_menu)
 
 songlist = Listbox(window, bg= "black" , fg= "white" , width=100, height= 22)
 songlist.pack()
@@ -33,24 +86,19 @@ pause_btn_image = PhotoImage(file='pause.png')
 next_btn_image = PhotoImage(file='next.png')
 previous_btn_image = PhotoImage(file='previous.png')
 
-control_frame =  Frame(window)
-control_frame.pack()
+control_frame = customtkinter.CTkFrame(window)
+control_frame.pack(pady=0, padx=0, fill="both", expand=True)
 
-play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
+play_btn = customtkinter.CTkButton(control_frame, image=play_btn_image, text='', command=play_music)
 play_btn.grid(row=0, column=0, padx=7, pady=10)
 
-pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
+pause_btn = customtkinter.CTkButton(control_frame, image=pause_btn_image, text='', command=pause_music)
 pause_btn.grid(row=0, column=2, padx=7, pady=10)
 
-next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
+next_btn = customtkinter.CTkButton(control_frame, image=next_btn_image, text='', command=next_music)
 next_btn.grid(row=0, column=3, padx=7, pady=10)
 
-previous_btn = Button(control_frame, image=previous_btn_image, borderwidth=0)
+previous_btn = customtkinter.CTkButton(control_frame, image=previous_btn_image, text='', command=previous_music)
 previous_btn.grid(row=0, column=1, padx=7, pady=10)
-
-pygame.mixer.music.load(r"C:\Users\DELL\Music\kyon.mp3")
-
-pygame.mixer.music.play()
-
 
 window.mainloop()
